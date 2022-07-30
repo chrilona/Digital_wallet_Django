9d7cd1b3fb6a356d8dab68ceb3fabff74861f2c5
chrilona <christabelalona@gmail>
2022-07-30 21:21:30 +0300

    Created and implemented different models for this digital wallet

---
+++ Wallet/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Account, Customer, Notification, Receipt, Thirdparty, Transaction, Wallet,Account,Card,Thirdparty,Notification,Receipt,Loan,Reward
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display= ("firstname","lastname","adress",)
+    search_fields= ("firstname","lastname",)
+
+admin.site.register(Customer,CustomerAdmin)
+admin.site.register(Wallet)
+admin.site.register(Account)
+admin.site.register(Card)
+admin.site.register(Thirdparty)
+admin.site.register(Notification)
+admin.site.register(Receipt)
+admin.site.register(Loan)
+admin.site.register(Reward)
+admin.site.register(Transaction)
+
+
+# Register your models here.
---
+++ Wallet/models.py
@@ -0,0 +1,101 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    firstname=models.CharField(max_length=15,null=True)
+    lastname=models.CharField(max_length=15,null=True)
+    address=models.CharField(default=1,max_length=100,null=True)
+    emails=models.EmailField(max_length=30,null=True)
+    phonenumber=models.CharField(max_length=10,null=True)
+    age=models.IntegerField(null=True)
+    gender=models.TextChoices
+
+    def __str__(self):
+        return self.firstname
+        
+class Wallet(models.Model):
+    balance=models.BigIntegerField
+    date_created = models.DateTimeField(max_length=30,auto_now=False)
+    adress=models.CharField(max_length=15)
+    isactive=models.BooleanField(max_length=5)
+    Customer=models.OneToOneField
+    balance=models.IntegerField
+    pin=models.SmallIntegerField
+    currency=models.CharField(max_length=8)
+
+class Account(models.Model):
+    account_number = models.IntegerField()
+    account_name=models.CharField(max_length=14)
+    account_type=models.CharField
+    balance=models.IntegerField
+    wallet=models.ForeignObject
+
+class Transaction(models.Model):
+    transaction_code = models.IntegerField
+    transaction_amount = models.BigIntegerField
+    transaction_charges = models.IntegerField
+    transaction_date_time = models.DateTimeField
+    origin_account = models.ForeignKey
+    destination_account = models.ForeignKey
+    Receipt = models.OneToOneField
+
+class Card(models.Model):
+    serial_number =models.IntegerField()
+    date_issued = models.DateTimeField(max_length=12,auto_now=False)
+    expiry_date=models.DateTimeField(max_length=12,auto_now=False)
+    signature=models.ImageField
+    Account=models.ForeignKey
+    Wallet=models.ForeignKey
+    issuer=models.CharField
+
+class Thirdparty(models.Model):
+    name=models.CharField
+    currency=models.ForeignKey
+    transaction_cost=models.IntegerField
+    Account=models.OneToOneField
+
+class Notification(models.Model):
+    description=models.TextField
+    title=models.CharField(max_length=8)
+    recipient=models.ForeignKey
+    read_unread_status=models.CharField
+    date_and_time=models.DateTimeField(auto_now=False)
+
+class Receipt(models.Model):
+    receipt_date=models.DateTimeField(auto_now=False)
+    receipt_description=models.TextField
+    name=models.CharField(max_length=12)
+    balance=models.BigIntegerField
+    amount=models.BigIntegerField
+    file=models.FileField
+
+class Loan(models.Model):
+    loan_id=models.IntegerField
+    loan_type=models.CharField
+    amount=models.IntegerField
+    loan_status=models.CharField
+    name=models.CharField
+    purpose_of_the_loan=models.CharField
+    Wallet=models.OneToOneField
+    date_issued=models.DateTimeField(auto_now=False)
+    payment_due_date=models.DateTimeField(auto_now=False)
+    loan_balance=models.IntegerField
+    interest_rate=models.SmallIntegerField
+    guarantor=models.ForeignKey
+    
+class Reward(models.Model):
+    date_of_reward=models.DateTimeField(auto_now=False)
+    description_of_reward=models.TextField
+    reward_points=models.SmallIntegerField
+    Wallet=models.ForeignKey
+
+
+
+
+
+
+
+
+
+
+
